fce565ba534969ef1ec6f9eb50fa5076d3ab9852
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-06-30 10:32:26 +0100

    added the button function, changed the key and gave it an id.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,22 +1,38 @@
-/* eslint-disable array-callback-return */
-import React from 'react';
-import buttons from './ButtonsKey';
+/* eslint-disable no-self-assign */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-typos */
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-export default class Buttons extends React.Component {
+export default class Buttons extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    [this.btn, this.id, this.clickEvent] = [props.btn, props.id, props.clickEvent];
+    this.clickEvent = this.clickEvent.bind(this);
+  }
+
+  clickEvent() {
+    const { clickEvent } = this.props;
+    clickEvent();
   }
 
   render() {
     return (
-      <div className="container-buttons">
-        {buttons.map((button) => (
-          <button className="button" type="button" key={button}>
-            {button}
-          </button>
-        ))}
-      </div>
+      <button
+        className="button"
+        type="button"
+        onClick={this.clickEvent}
+        key={this.id}
+        id={this.id}
+      >
+        {this.btn}
+      </button>
     );
   }
 }
+
+Buttons.propTypes = {
+  btn: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  clickEvent: PropTypes.func.isRequired,
+};
