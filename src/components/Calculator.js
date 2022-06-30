db9af539fed9153a55d79ff66d016276a152cc06
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-06-30 10:36:17 +0100

    brought the upper and the keybuttons together and added necessary function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Buttons from './Buttons';
+import KeyOperation from './keyOperation';
 import Upper from './Upper';
+import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class Calculator extends React.Component {
+export default class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.clickEvent = this.clickEvent.bind(this);
+    this.state = {
+      total: 0,
+      operation: null,
+      next: null,
+    };
+  }
+
+  clickEvent(e) {
+    this.setState((state) => {
+      const result = calculate(state, e.target.textContent);
+      return result;
+    });
   }
 
   render() {
+    const { next, operation } = this.state;
+    let { total } = this.state;
+    total = total === null && next ? '' : total;
     return (
       <div className="main-container">
         <div className="container">
-          <Upper />
-          <Buttons />
+          <Upper
+            operation={operation || ''}
+            total={total === null ? 0 : total}
+            next={next || ''}
+          />
+          <KeyOperation
+            clickEvent={this.clickEvent}
+          />
         </div>
       </div>
     );
