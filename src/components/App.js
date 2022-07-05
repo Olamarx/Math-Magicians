2c564b619f7f577ceeaaeca123d89e069568dd8f
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-05 19:19:00 +0100

    Added the routes by importing it

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
+/* eslint-disable constructor-super */
+/* eslint-disable no-empty-function */
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './Calculator';
-
+import Quote from './Quote';
+import Home from './Home';
+// import Nav from './Nav';
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  constructor() {
   }
 
   render() {
-    return <Calculator />;
+    return (
+      <>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quotes" element={<Quote />} />
+        </Routes>
+      </>
+    );
   }
 }
