fb178c07f37cd08c93a420a5ed7f9c1f4997be75
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-05 23:24:42 +0100

    Added a homepage for the site.

---
+++ src/components/Home.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import Nav from './Nav';
+import styling from './Home.module.css';
+
+function Home() {
+  return (
+    <>
+      <Nav />
+      <section>
+        <article className={styling.home}>
+          <h2>Welcome to the Magician&#39;s Page</h2>
+
+          <p>
+            If you are asking about how someone feels
+            about you, and you draw the  Magician, that means
+            that this person feels that you are very special, and that you might be
+            “the One.” They feel that they can talk to you about
+            anything and everything, and that you stimulate and satisfy them on all
+            levels: physically (pentacles/earth), emotionally
+            (cups/water), spiritually (wands/fire), and
+            intellectually (swords/air). They feel that
+            you understand them at a very deep level.
+            Their feelings for you go beyond just
+            potential, because they see your
+            relationship manifesting in reality.
+            This is real, and this is amazing!
+            <br />
+            The Magician as feelings means that they
+            feel charmed and inspired by you.
+            You make them feel magical. They feel excited
+            and happy to be alive. They feel creative and in
+            touch with their “why” and their intention,
+            extending to the depths of their soul. You
+            make them feel confident in themselves and in
+            their relationship with you. They have a vision
+            for where you are in their life, and a
+            vision for where you both can go together.
+            <br />
+            For singles and those embarking on new
+            relationships, the Magician can be a sign that
+            someone has fallen head over heels for you and
+            is eager to take your relationship to the next
+            level. Unlike the Fool, the Magician’s feelings
+            go beyond butterflies and initial infatuation.
+            It’s so easy for the two of you to communicate.
+            They are excited to work together with you to
+            create a mutually satisfying relationship,
+            founded on a shared vision.
+            <br />
+            For existing relationships, the Magician means
+            that they truly feel that you are their
+            partner in life and soulmate. You’ve
+            experienced so much together and you have
+            been there for each other, through
+            thick and thin. You’ve been there in the
+            trenches for each other. You were there
+            for them when they had nothing. You created
+            this life together, and it’s a sight to be seen.
+            They admire you and how you’ve worked together
+            to create a beautiful and abundant relationship.
+            They feel excited to enjoy the fruits of your labor.
+            <br />
+            If you are asking about an old flame or an ex’s feelings
+            about you, the Magician can symbolize that their old
+            feelings for you have been reignited. They remember
+            what you shared and what you had together, and how
+            special your relationship was. They may feel
+            that the grass is not greener on the other side.
+            They feel an urge to return to you. It’s
+            possible that they will reach out soon. If they
+            do, they will likely make their intentions
+            very clear, because the Magician is all
+            about manifestation.
+            <br />
+            In the context of whether or not a relationship
+            is headed for commitment, engagement, or marriage,
+            the Magician as feelings says that they are looking
+            to take your relationship to the next level. As a
+            card of manifestation, it is all about having a vision and
+            turning dreams into reality. Will they propose?
+            The Magician suggests yes, because they see a future
+            with you and want to do what it takes to make it happen.
+          </p>
+
+          <h3>
+            extract from Tarot Card Meaning: Magician as Feelings
+            By Sibyl Tarot
+
+          </h3>
+        </article>
+      </section>
+    </>
+  );
+}
+
+export default Home;
